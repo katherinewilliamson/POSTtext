690e49862670db04b7c55ec1753fcb571f5ae643
Katherine Williamson <152924374+katherinewilliamson@users.noreply.github.com>
2024-01-19 16:16:05 -0600

    Initiated script file
    Created file and defined preliminary function to parse a singular data file into a dictionary.

---
+++ run.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+import sys
+import argparse
+
+# Indicates accepted command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--directory")
+parser.add_argument("-uri", "--uri")
+parser.add_argument("-f", "--fields")
+
+args = parser.parse_args()
+directory = args.directory
+uri = args.uri
+# Converts string of fields into a list and strips each entry of blank characters if present
+fields = args.fields.split(",")
+for index, item in enumerate(fields):
+    fields[index] = item.strip()
+    
+# Function parses the contents of a passed data file into a list of elements and builds a dictionary for a POST request with provided fields
+def dataparse(datafile):
+    with open(datafile, "r") as data:
+        parsedData = data.readlines()
+    if len(parsedData) != len(fields):
+        raise Exception("Error parsing file {}: Field-value error.".format(datafile))
+    requestData = {}
+    for index, field in enumerate(fields):
+        requestData[field] = parsedData[index]
+    return requestData
