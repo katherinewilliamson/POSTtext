6152ec40fac4444079bd23d0b9aabb57de159286
Katherine Williamson <152924374+katherinewilliamson@users.noreply.github.com>
2024-01-20 03:01:09 -0600

    Formatting fix

--- run.py
+++ run.py
@@ -13,19 +13,19 @@ parser.add_argument("-f", "--fields")
 args = parser.parse_args()
 directory = args.directory
 uri = args.uri
-# Converts string of fields into a list and strips each entry of blank chara>
+# Converts string of fields into a list and strips each entry of blank characters if present
 fields = args.fields.split(",")
 for index, item in enumerate(fields):
     fields[index] = item.strip()
 
-# Function parses the contents of a passed data file into a list of elements>
+# Function parses the contents of a passed data file into a list of elements and builds a dictionary for a POST request with provided fields
 def dataparse(datafile):
     datafile = os.path.join(directory, datafile)
     with open(datafile, "r") as data:
         parsedData = data.readlines()
     if len(parsedData) != len(fields):
-        raise Exception("Error parsing file {}: Field-value error.".format(d>
-    requestData = {}
+        raise Exception("Error parsing file {}: Field-value error.".format(datafile))
+        requestData = {}
     for index, field in enumerate(fields):
         requestData[field] = parsedData[index].strip()
     return requestData
@@ -34,7 +34,7 @@ def dataparse(datafile):
 def postrequest(requestData):
     response = requests.post(uri, json=requestData)
     if response.ok:
-        print(f"Request completed successfully for {len(requestData)} files.>
+        print(f"Request completed successfully for {len(requestData)} files.")
     else:
         print(f"Request failed with status code {response.status_code}")
 
